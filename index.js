9746d8d0af1ee43687401e4043093822969f8a38
unknown <nir@nir-lap.il.webcollage.com>
2018-01-08 08:55:17 +0200

    update

--- index.js
+++ index.js
@@ -14,11 +14,6 @@ app.use( (req, res, next) => {
     next();
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 
 app.get('/getbooks', (req, res) => {
